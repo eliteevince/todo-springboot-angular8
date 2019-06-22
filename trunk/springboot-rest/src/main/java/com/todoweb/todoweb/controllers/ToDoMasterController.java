89771f425d51ae8a5988e5b59b5d8a4ae708056a
Jaydeep <jaydeep@eliteevince.com>
2019-06-22 21:07:05 +0530

    - changed url to *.

--- trunk/springboot-rest/src/main/java/com/todoweb/todoweb/controllers/ToDoMasterController.java
+++ trunk/springboot-rest/src/main/java/com/todoweb/todoweb/controllers/ToDoMasterController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/todo")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 public class ToDoMasterController {
 
     @Autowired
